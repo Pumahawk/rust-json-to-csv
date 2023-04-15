67cf25f3282f78d720fc829f49ae8bb4da358f12
Lorenzo Gandino <lorenzoga94@gmail.com>
2023-04-15 15:32:30 +0200

    gestione config, print header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::env;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let config = config().expect("Invalid arguments");
+    pring_header(&config);
+
+    let input = io::stdin();
 }
+
+type Result<T> = std::result::Result<T, &'static str>;
+
+struct Config {
+    header: bool,
+    columns: Vec<(String, String)>, // name, path
+}
+
+fn config() -> Result<Config> {
+    let mut arg_counter = -1;
+    let mut header = true;
+    let mut columns = Vec::new();
+    let mut args = env::args().skip(1);
+    while let Some(arg) = args.next() {
+        arg_counter += 1;
+        let argtmp: &str = &arg;
+        match &argtmp {
+            &"--no-head" => header = false,
+            &"-c" => columns.push((
+                args.next().ok_or("Not found key argument")?,
+                args.next().ok_or("Not found path argument")?
+            )),
+            _ => columns.push((arg_counter.to_string(), arg)),
+        }
+    }
+    Ok(Config {
+        header,
+        columns,
+    })
+}
+
+fn pring_header(config: &Config) {
+    if config.header {
+        let header = config.columns
+            .iter()
+            .map(|el|&el.0)
+            .map(|el|el.as_str())
+            .collect::<Vec<_>>()
+            .join(",");
+        println!("{}", header);
+    }
+}
+
