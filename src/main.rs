75d4f3d177d65c9ecdcd39fbf54adc2db23b08a4
Lorenzo Gandino <lorenzoga94@gmail.com>
2023-04-17 18:32:00 +0200

    AAggiunta funzionalità di escape in output

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,14 @@ type Result<T> = std::result::Result<T, &'static str>;
 
 struct Config {
     header: bool,
+    string_escape: bool,
     columns: Vec<(String, String)>, // name, path
 }
 
 fn config() -> Result<Config> {
     let mut arg_counter = -1;
     let mut header = true;
+    let mut escape = true;
     let mut columns = Vec::new();
     let mut args = env::args().skip(1);
     while let Some(arg) = args.next() {
@@ -30,6 +32,7 @@ fn config() -> Result<Config> {
         let argtmp: &str = &arg;
         match &argtmp {
             &"--no-head" => header = false,
+            &"--no-escape" => escape = false,
             &"-c" => columns.push((
                 args.next().ok_or("Not found key argument")?,
                 args.next().ok_or("Not found path argument")?
@@ -40,6 +43,7 @@ fn config() -> Result<Config> {
     Ok(Config {
         header,
         columns,
+        string_escape: escape,
     })
 }
 
@@ -63,13 +67,22 @@ fn map_row(config: &Config, line: String) -> String {
     let mut chars = line.chars();
     let object = json::parser(&mut chars).expect("Unable to read object from line").into();
     let reader = json::ReaderJson::new(&object);
-    config.columns.iter().map(|(_, path)| path)
-        .map(|path| match reader.path(path).json() {
-            TypeJson::Object(_) => String::from("[object_json]"),
-            TypeJson::List(_) => String::from("[list_json]"),
-            TypeJson::Text(txt) => txt.to_string(),
-            TypeJson::Number(n) => n.to_string(),
-            TypeJson::Null => String::from(""),
+    config.columns
+        .iter()
+        .map(|(_, path)| path)
+        .map(|path|reader.path(path))
+        .map(|obj| {
+            if config.string_escape {
+                obj.json().to_string()
+            } else {
+                match obj.json() {
+                    TypeJson::Object(_) => String::from("[object_json]"),
+                    TypeJson::List(_) => String::from("[list_json]"),
+                    TypeJson::Text(txt) => txt.to_string(),
+                    TypeJson::Number(n) => n.to_string(),
+                    TypeJson::Null => String::from(""),
+                }
+            }
         })
         .collect::<Vec<_>>()
         .join(",")
